80de470fe1952e80d156fd248c9230b91b6c1191
sanjeev kumar <sanjeev13982@gmail.com>
2023-12-19 22:40:17 +0530

    weblogin

---
+++ script1.js
@@ -0,0 +1,11 @@
+// You can add JavaScript code here if needed
+// For example, handling form submission or adding more interactivity
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+
+    // Add a class to trigger the animation
+    loginForm.classList.add('animated');
+
+    // You can customize the animation duration and easing function
+    loginForm.style.animation = 'slideIn 1s ease-in-out';
+});
